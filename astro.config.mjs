8cd888872009d370efac87ea4df9292cbd15d9b0
Brian Geertsma <39035211+brian-gee@users.noreply.github.com>
2023-04-17 19:21:11 -0400

    updating with deno rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify/functions";
+
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: netlify()
+  adapter: deno()
 });

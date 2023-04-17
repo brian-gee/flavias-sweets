8ce87b64d577d61ceb959e7d4346919218ff68b6
Brian Geertsma <39035211+brian-gee@users.noreply.github.com>
2023-04-17 19:37:54 -0400

    updating to try google cloud ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify/functions";
 import deno from "@astrojs/deno";
 
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: netlify()
+  adapter: node({
+    mode: "standalone"
+  })
 });

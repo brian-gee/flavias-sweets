6eac94e3aacce7a4a133404d7412cf553fdcad8f
Brian Geertsma <39035211+brian-gee@users.noreply.github.com>
2023-04-18 03:51:41 -0400

    updating

--- src/utils/initFirebase.js
+++ src/utils/initFirebase.js
@@ -4,15 +4,21 @@ import { v4 as uuidv4 } from 'uuid';
 
 const db = getDatabase(app);
 
-export const addMessage = (name, email, message) => {
+// Add customer
+export const addMessage = async (name, email, message) => {
   const messageId = uuidv4();
   const dateTime = new Date().toISOString();
 
-  set(ref(db, 'messages/' + dateTime), {
-    dateTime: dateTime,
-    messageId: messageId,
-    name: name,
-    email: email,
-    message: message,
-  });
+  try {
+    await set(ref(db, 'messages/' + messageId), {
+      dateTime: dateTime,
+      messageId: messageId,
+      name: name,
+      email: email,
+      message: message,
+    });
+    console.log(dateTime, messageId, name, email, message, 'added');
+  } catch (error) {
+    console.error('Error adding message:', error);
+  }
 };

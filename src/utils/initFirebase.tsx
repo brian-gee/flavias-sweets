07ecab386a48583c460fef766d7b893dc549de97
Brian Geertsma <39035211+brian-gee@users.noreply.github.com>
2023-04-18 02:37:40 -0400

    updating function for saving message

--- src/utils/initFirebase.tsx
+++ src/utils/initFirebase.tsx
@@ -1,12 +1,15 @@
 import { app } from './initAuth';
 import { getDatabase, ref, set } from 'firebase/database';
+import { v4 as uuidv4 } from 'uuid';
+
+const messageId = uuidv4();
 
 const db = getDatabase(app);
 const dbRef = ref(db, 'contactForm');
 
 // Add customer
 export const addMessage = (name: string, email: string, message: string) => {
-  set(ref(db), {
+  set(ref(db, 'messages/' + messageId), {
     name: name,
     email: email,
     message: message,

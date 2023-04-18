cfe0dcfe9fd624af8fc94646af66678776c78779
Brian Geertsma <39035211+brian-gee@users.noreply.github.com>
2023-04-18 02:46:18 -0400

    retrying

--- src/utils/initFirebase.tsx
+++ src/utils/initFirebase.tsx
@@ -1,14 +1,12 @@
 import { app } from './initAuth';
 import { getDatabase, ref, set } from 'firebase/database';
-import { v4 as uuidv4 } from 'uuid';
-
-const messageId = uuidv4();
 
 const db = getDatabase(app);
+const dbRef = ref(db, 'contactForm');
 
 // Add customer
 export const addMessage = (name: string, email: string, message: string) => {
-  set(ref(db, 'messages/' + messageId), {
+  set(dbRef, {
     name: name,
     email: email,
     message: message,

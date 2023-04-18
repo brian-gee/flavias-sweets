1c2a6307788671d152dff12d9000cec77cff0fd1
Brian Geertsma <39035211+brian-gee@users.noreply.github.com>
2023-04-17 20:31:46 -0400

    updating for docker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify/functions";
-import deno from "@astrojs/deno";
+import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify/functions';
 
-import node from "@astrojs/node";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
+  output: 'server',
   adapter: node({
-    mode: "standalone"
-  })
+    mode: 'standalone',
+  }),
 });

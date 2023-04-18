b40a2695cd7bbec5934cfbf277c033361d7a0335
Brian Geertsma <39035211+brian-gee@users.noreply.github.com>
2023-04-17 21:35:49 -0400

    updating back

--- src/components/instagram.js
+++ src/components/instagram.js
@@ -4,7 +4,12 @@ export async function fetchInstagramPosts(numberOfPosts) {
     const response = await fetch(
       `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,thumbnail_url,video_url,permalink&access_token=${ACCESSTOKEN}&limit=${numberOfPosts}`
     );
-    const data = await response.json();
+
+    // Log the raw response text
+    const responseText = await response.text();
+    console.log('Raw response:', responseText);
+
+    const data = JSON.parse(responseText);
     if (data.error) {
       throw new Error(data.error.message);
     }

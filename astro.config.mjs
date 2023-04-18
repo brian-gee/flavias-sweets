0e994d282035498ad0fb8b47706100e1982fffeb
Brian Geertsma <39035211+brian-gee@users.noreply.github.com>
2023-04-17 22:24:23 -0400

    preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify/functions';
 import react from "@astrojs/react";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), preact()],
   output: 'server',
   adapter: netlify()
 });

f5446ffda6bf40d40283179b938ef5626504f5af
Brian Geertsma <39035211+brian-gee@users.noreply.github.com>
2023-04-17 21:41:23 -0400

    updating

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify/functions';
-import node from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: netlify()
+  adapter: netlify(),
 });

075e4381b6a3a44130c739a48c0db8b7cfbcf34f
Brian Geertsma <39035211+brian-gee@users.noreply.github.com>
2023-04-18 03:02:06 -0400

    updating function

--- src/utils/initFirebase.js
+++ src/utils/initFirebase.js
@@ -2,12 +2,12 @@ import { app } from './initAuth';
 import { getDatabase, ref, set } from 'firebase/database';
 import { v4 as uuidv4 } from 'uuid';
 
-const messageId = uuidv4();
-
 const db = getDatabase(app);
 
 // Add customer
 export const addMessage = (name, email, message) => {
+  const messageId = uuidv4();
+
   set(ref(db, 'messages/' + messageId), {
     messageId: messageId,
     name: name,

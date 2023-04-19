755e2d0d8c9ec5eda3ee20dfda5f60af447abf8c
Brian Geertsma <39035211+brian-gee@users.noreply.github.com>
2023-04-18 22:55:29 -0400

    updating

--- functions/submitForm.js
+++ functions/submitForm.js
@@ -1,4 +1,49 @@
-const { addMessage } = require('./initFirebase');
+import { initializeApp } from 'firebase/app';
+import { app } from './initAuth';
+import { getFirestore, doc, setDoc } from 'firebase/firestore';
+import { v4 as uuidv4 } from 'uuid';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBynkTp6c_sEhEF6e-EZ7fLayy28_i70YI',
+  authDomain: 'flavias-sweets.firebaseapp.com',
+  databaseURL: 'https://flavias-sweets-default-rtdb.firebaseio.com',
+  projectId: 'flavias-sweets',
+  storageBucket: 'flavias-sweets.appspot.com',
+  messagingSenderId: '6930398200',
+  appId: '1:6930398200:web:31b9d3eeedd904e0e6340d',
+  measurementId: 'G-ECC718YS2N',
+};
+
+export const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const userEmail = 'briangeertsma@gmail.com';
+
+// Add customer
+export const addMessage = async (
+  firstName,
+  lastName,
+  email,
+  phoneNumber,
+  message
+) => {
+  const messageId = uuidv4();
+  const dateTime = new Date().toISOString();
+
+  try {
+    await setDoc(doc(db, 'mail/' + messageId), {
+      dateTime: dateTime,
+      messageId: messageId,
+      to: userEmail,
+      message: {
+        subject: "New message from Flavias's Sweets!",
+        text: message,
+      },
+    });
+  } catch (error) {
+    console.error('Error adding message:', error);
+  }
+};
 
 exports.handler = async function (event, context) {
   try {

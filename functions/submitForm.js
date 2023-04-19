9706c4524390c4d383eb17f6282ab0c108dd7389
Brian Geertsma <39035211+brian-gee@users.noreply.github.com>
2023-04-18 21:05:57 -0400

    updating

--- functions/submitForm.js
+++ functions/submitForm.js
@@ -1,13 +1,5 @@
-import { addMessage } from './initFirebase';
-
 exports.handler = async function (event, context) {
   try {
-    const data = JSON.parse(event.body);
-    const { name, email, password } = data;
-
-    // Call addMessage only when there are no validation errors
-    await addMessage(name, email, password);
-
     return {
       statusCode: 200,
       body: JSON.stringify({ message: 'Message added successfully.' }),

585bfcd0644cd1ac4094359ca33e855cadd9dff3
brian <brian@theonemini.attlocal.net>
2023-04-22 00:05:00 -0400

    updating with react

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify/functions';
 
-import svelte from "@astrojs/svelte";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), react()],
   output: 'server',
-  adapter: netlify()
+  adapter: netlify(),
 });

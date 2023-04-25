7eed56b68e3b7ae1d586d448d62fedcde81505e6
Brian Geertsma <39035211+brian-gee@users.noreply.github.com>
2023-04-25 15:30:11 -0400

    updating title layout, working on hero

--- src/utils/instagram.js
+++ src/utils/instagram.js
@@ -5,9 +5,26 @@ export async function fetchInstagramPosts(numberOfPosts) {
       `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,thumbnail_url,video_url,permalink&access_token=${ACCESSTOKEN}&limit=${numberOfPosts}`
     );
 
-    // Log the raw response text
     const responseText = await response.text();
-    console.log('Raw response:', responseText);
+
+    const data = JSON.parse(responseText);
+    if (data.error) {
+      throw new Error(data.error.message);
+    }
+    return data.data;
+  } catch (error) {
+    console.error('Error fetching Instagram posts:', error);
+    return [];
+  }
+}
+
+export async function fetchIndividualPost() {
+  try {
+    const response = await fetch(
+      `https://graph.instagram.com/17883980327721176?fields=id,caption,media_type,media_url,thumbnail_url,video_url,permalink&access_token=${ACCESSTOKEN}`
+    );
+
+    const responseText = await response.text();
 
     const data = JSON.parse(responseText);
     if (data.error) {

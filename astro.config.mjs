e497bc67f4d15d5bb5aae78743515b532c93ca10
Brian Geertsma <39035211+brian-gee@users.noreply.github.com>
2023-05-01 21:21:07 -0400

    updating dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify/functions';
-import react from '@astrojs/react';
 import image from '@astrojs/image';
 
 // https://as/*  */tro.build/config
@@ -9,7 +8,6 @@ export default defineConfig({
   site: 'https://flavias-sweets.netlify.app',
   integrations: [
     tailwind(),
-    react(),
     image({
       serviceEntryPoint: '@astrojs/image/sharp',
     }),

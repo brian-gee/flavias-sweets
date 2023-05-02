52d1be92ed3c29ba4dd339a477f3f11e28e1058b
Brian Geertsma <39035211+brian-gee@users.noreply.github.com>
2023-05-02 10:21:33 -0400

    updating

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify/functions';
-<<<<<<< HEAD
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
@@ -12,14 +11,3 @@ export default defineConfig({
   integrations: [tailwind(), sitemap(), netlify()],
   output: 'server',
 });
-=======
-
-import react from "@astrojs/react";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: netlify()
-});
->>>>>>> parent of db10358 (updating with lighthouse improvements)

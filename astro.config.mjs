5bbe9f155c24eac8fac8036fe64c813415741df1
Brian Geertsma <39035211+brian-gee@users.noreply.github.com>
2023-05-01 22:46:51 -0400

    updating test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,11 @@ import netlify from '@astrojs/netlify/functions';
 import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
 
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://flavias-sweets.netlify.app',
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp',
-    }),
-    sitemap(),
-  ],
+  integrations: [tailwind(), sitemap(), netlify(), image()],
+  output: "server",
+  adapter: netlify()
 });

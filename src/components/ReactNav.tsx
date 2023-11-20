dee173114be1b5a9641041421287ce0efba16629
brian-gee <39035211+brian-gee@users.noreply.github.com>
2023-11-19 22:25:03 -0500

    working mobile nav

--- src/components/ReactNav.tsx
+++ src/components/ReactNav.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import Logo from './Logo';
 import { FaFacebook, FaInstagram } from 'react-icons/fa';
+import { TiThMenu } from 'react-icons/ti';
+import { IoMdClose } from 'react-icons/io';
 
 const NavLinks = () => {
   return (
-    <ul className="flex space-x-8 font-bold text-lg">
+    <ul className="hidden lg:flex space-x-8 font-bold text-lg">
       <a href="/" className="hover:text-pink-500">
         <li>Home</li>
       </a>
@@ -24,15 +26,41 @@ const NavLinks = () => {
 const NavIcons = () => {
   return (
     <ul className="flex space-x-4 font-bold text-lg">
-      <a href="/" className="hover:text-pink-500">
+      <a
+        href="https://www.facebook.com/Flaviasweets/"
+        className="hover:text-pink-500">
         <li>
-          <FaFacebook className="w-7 h-7" />
+          <FaFacebook className="w-6 h-6" />
         </li>
       </a>
-      <a href="/" className="hover:text-pink-500">
+      <a
+        href="https://www.instagram.com/flavias_sweets/"
+        className="hover:text-pink-500">
         <li>
-          <FaInstagram className="w-7 h-7" />
+          <FaInstagram className="w-6 h-6" />
         </li>
+      </a>
+    </ul>
+  );
+};
+
+const MobileNav = ({ toggleMobileNav }) => {
+  return (
+    <ul className="lg:hidden fixed inset-0 bg-white flex flex-col items-center space-y-2 p-6">
+      <button onClick={toggleMobileNav} className="self-end px-4 py-2 text-lg">
+        <IoMdClose />
+      </button>
+      <a href="/" className="hover:text-pink-500">
+        <li>Home</li>
+      </a>
+      <a href="/about" className="hover:text-pink-500">
+        <li>About</li>
+      </a>
+      <a href="/gallery" className="hover:text-pink-500">
+        <li>Gallery</li>
+      </a>
+      <a href="/meet" className="hover:text-pink-500">
+        <li>Meet Flavia</li>
       </a>
     </ul>
   );
@@ -40,12 +68,23 @@ const NavIcons = () => {
 
 export default function ReactNav() {
   const [isOpen, setIsOpen] = useState(false);
+  const toggleMobileNav = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <header className="xl:px-2">
-      <div className="sticky top-0 z-[20] mx-auto flex max-w-7xl items-center justify-between">
+    <header className="px-3 sm:px-6 py-10">
+      <div className="sticky top-0 z-[20] mx-auto flex max-w-3xl lg:max-w-6xl items-center justify-between">
         <Logo />
         <NavLinks />
-        <NavIcons />
+        <div className="flex items-center space-x-4">
+          <NavIcons />
+
+          <button onClick={toggleMobileNav}>
+            <TiThMenu className="lg:hidden text-pink-500 w-6 h-6" />
+          </button>
+          {isOpen && <MobileNav toggleMobileNav={toggleMobileNav} />}
+        </div>
       </div>
     </header>
   );

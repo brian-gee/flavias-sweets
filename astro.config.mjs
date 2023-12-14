7670226a408b5370931c37ade39f2778cb490616
brian-gee <39035211+brian-gee@users.noreply.github.com>
2023-12-13 22:14:16 -0500

    updating config for react-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import tailwind from '@astrojs/tailwind';
 import netlify from '@astrojs/netlify/functions';
 import sitemap from '@astrojs/sitemap';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
   site: 'https://flavias-sweets.netlify.app',
-  integrations: [tailwind(), sitemap(), netlify(), react()]
+  integrations: [tailwind(), sitemap(), netlify(), react()],
+  vite: {
+    ssr: {
+      noExternal: ['react-icons'],
+    },
+  },
 });

68f2cf99f31a7a569a227a860cb01f498965205b
brian-gee <39035211+brian-gee@users.noreply.github.com>
2023-12-15 09:53:16 -0500

    updating astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
-  site: 'https://flavias-sweets.netlify.app',
+  site: 'https://flaviasweets.web.app/',
   integrations: [tailwind(), sitemap(), netlify(), react()],
   vite: {
     ssr: {

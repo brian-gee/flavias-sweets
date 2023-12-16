4cd2ccea2149d968d279bb4ecf3e3bde557c899c
brian-gee <39035211+brian-gee@users.noreply.github.com>
2023-12-16 16:56:59 -0500

    removing icon package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    ssr: {
-      noExternal: ["react-icons"],
-    },
-  },
-  integrations: [tailwind(), react()],
+  integrations: [tailwind()]
 });
